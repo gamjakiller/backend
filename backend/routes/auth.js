677ebd14dfc6d845cecc6238e41026990eeea99e
gamjakiller <156633316+gamjakiller@users.noreply.github.com>
2024-03-06 15:07:56 +0900

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { validateLogin } = require('../middlewares/validation')
-const authController = require('../controllers/login')
-
-// UPDATE
-router.post('/login', validateLogin, authController.login)
-
-module.exports = router
